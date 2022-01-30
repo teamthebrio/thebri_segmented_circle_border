fa4b8ea6fe5f3c72b1e65c03f8ab8d456875802b
Raza Khan <thisisrazakhan8@gmail.com>
2022-01-30 18:20:22 +0530

    Inital Commit

---
+++ README.md
@@ -0,0 +1,54 @@
+<!-- 
+This README describes the package. If you publish this package to pub.dev,
+this README's contents appear on the landing page for your package.
+
+For information about how to write a good package README, see the guide for
+[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
+
+For general information about developing packages, see the Dart guide for
+[creating packages](https://dart.dev/guides/libraries/create-library-packages)
+and the Flutter guide for
+[developing packages and plugins](https://flutter.dev/developing-packages). 
+-->
+
+# TheBrio Segmented Circle Border!!
+A package for creating segmented circle borders in flutter.
+
+## Getting started
+Add the package to your ```pubspec.yaml```:
+
+```thebri_segmented_circle_border: 0.0.1```
+
+## Usage
+
+Add it as a border:
+```dart
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:thebri_segmented_circle_border/thebri_segmented_circle_border.dart';
+
+void main() => runApp(SegmentedCircleBorderExample());
+
+class TheBrioSegmentedCircleBorderExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Material(
+      shape: TheBrioSegmentedCircleBorder(
+          offset: -(112.5 * pi / 180),
+          numberOfSegments: 8,
+          sides: <BorderSide>[
+            BorderSide(color: Color(0xFFFF0000), width: 10.0),
+            BorderSide(color: Color(0xFF00FF00), width: 1.0),
+            BorderSide(color: Color(0xFF0000FF), width: 2.0),
+            BorderSide(color: Color(0xFFFF0000), width: 3.0),
+            BorderSide(color: Color(0xFF00FF00), width: 4.0),
+            BorderSide(color: Color(0xFF0000FF), width: 5.0),
+            BorderSide(color: Color(0xFFFF00000), width: 6.0),
+            BorderSide(color: Color(0xFF00FF00), width: 7.0),
+          ]),
+      child: Container(
+        width: 100,
+        height: 100,
+      ));
+}
+```
---
+++ lib/thebri_segmented_circle_border.dart
@@ -0,0 +1,101 @@
+library thebri_segmented_circle_border;
+
+import 'dart:math';
+import 'package:flutter/painting.dart';
+
+/// A border that fits a segmented circle within the available space.
+class TheBrioSegmentedCircleBorder extends ShapeBorder {
+  /// Creates a segmented circle border.
+  ///
+  /// Length of [sides] must be less than [numberOfSegments].
+  // ignore: non_constant_identifier_names
+  TheBrioSegmentedCircleBorder(
+      {this.offset = 0.0, this.numberOfSegments, this.sides}) {
+    for (int i = sides!.length; i < numberOfSegments!; i++) {
+      sides!.add(BorderSide.none);
+    }
+    assert(sides != null);
+    assert(sides!.length <= numberOfSegments!);
+  }
+
+  /// The angle to offset each segment by. An offset of zero
+  /// radians meaning the first segment begins at the
+  /// point on the right hand side of the oval that
+  /// crosses the horizontal line that intersects the center of the
+  /// rectangle and with positive angles going clockwise around the
+  /// oval.
+  double? offset;
+
+  /// The number of segments the circle has. If fewer
+  /// sides are provided than the [numberOfSegments],
+  /// the remaining segments will be empty.
+  int? numberOfSegments;
+
+  /// The segments of the circle.
+  List<BorderSide>? sides;
+
+  @override
+  EdgeInsetsGeometry get dimensions =>
+      EdgeInsets.all(sides!.map((e) => e.width).reduce(max));
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) => Path()
+    ..addOval(Rect.fromCircle(
+        center: rect.center,
+        radius: max(0.0,
+            rect.shortestSide / 2.0 - sides!.map((e) => e.width).reduce(max))));
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    double widestSide = sides!.map((e) => e.width).reduce(max);
+    double innerRadius = rect.shortestSide / 2.0 - widestSide;
+    Path ret = Path();
+    double radiansPerSegment = 2 * pi / numberOfSegments!;
+    for (int i = 0; i < sides!.length; i++) {
+      ret.arcTo(
+          Rect.fromCircle(
+              center: rect.center, radius: innerRadius + sides![i].width),
+          i * radiansPerSegment + offset!,
+          radiansPerSegment,
+          false);
+      ret.lineTo(
+          rect.center.dx +
+              cos((i + 1) * radiansPerSegment + offset!) *
+                  (innerRadius + sides![(i + 1) % sides!.length].width),
+          rect.center.dy +
+              sin((i + 1) * radiansPerSegment + offset!) *
+                  (innerRadius + sides![(i + 1) % sides!.length].width));
+    }
+    return ret;
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+    double widestSide = sides!.map((e) => e.width).reduce(max);
+    double innerRadius = (rect.shortestSide - widestSide) / 2.0;
+    double radiansPerSegment = 2 * pi / numberOfSegments!;
+    for (int i = 0; i < sides!.length; i++) {
+      canvas.drawArc(
+        Rect.fromCircle(
+            center: rect.center,
+            radius: innerRadius - ((widestSide - sides![i].width) / 2.0)),
+        i * radiansPerSegment + offset!,
+        radiansPerSegment,
+        false,
+        sides![i].toPaint(),
+      );
+    }
+  }
+
+  @override
+  ShapeBorder scale(double t) => TheBrioSegmentedCircleBorder(
+      offset: offset,
+      numberOfSegments: numberOfSegments,
+      // sides: sides!.map((e) => e.scale(t)),
+      sides: sides!);
+
+  @override
+  String toString() {
+    return '$runtimeType($offset, $numberOfSegments, $sides)';
+  }
+}
